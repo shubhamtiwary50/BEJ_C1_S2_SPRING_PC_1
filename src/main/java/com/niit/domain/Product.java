dfb41fd6ece923920b9458828bd363054d85734f
SHUBHAM <shubhamtiwary50@gmail.com>
2023-02-21 13:36:06 +0530

    Added constructors in domain class Product

--- src/main/java/com/niit/domain/Product.java
+++ src/main/java/com/niit/domain/Product.java
@@ -7,5 +7,13 @@ public class Product {
     private double productPrice;
     private String productColor;
 
+    public Product() {
+    }
 
+    public Product(int productId, String productName, double productPrice, String productColor) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productColor = productColor;
+    }
 }
