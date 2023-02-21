c950a6e8993ac02da1787fd8e5f57a14f85b7664
SHUBHAM <shubhamtiwary50@gmail.com>
2023-02-21 13:34:47 +0530

    Added attributes in domain class

--- src/main/java/com/niit/domain/Product.java
+++ src/main/java/com/niit/domain/Product.java
@@ -1,4 +1,11 @@
 package com.niit.domain;
 
 public class Product {
+
+    private  int productId;
+    private String productName;
+    private double productPrice;
+    private String productColor;
+
+
 }
