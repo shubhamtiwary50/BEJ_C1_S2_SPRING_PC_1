f79a75ddddc7cbeb450190f7842b1991a60c5f17
SHUBHAM <shubhamtiwary50@gmail.com>
2023-02-21 13:48:09 +0530

    Added dependency.

--- src/main/java/com/niit/App.java
+++ src/main/java/com/niit/App.java
@@ -2,13 +2,10 @@ package com.niit;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+//         ApplicationContext applicationContext = new AnnotationCon
     }
 }
