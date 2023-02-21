7ed9a215bb167b395b414fa7760167a624dbfa67
SHUBHAM <shubhamtiwary50@gmail.com>
2023-02-21 14:21:40 +0530

    The main class of the app. Completed.

--- src/main/java/com/niit/App.java
+++ src/main/java/com/niit/App.java
@@ -1,11 +1,17 @@
 package com.niit;
 
+import com.niit.Config.BeanConfig;
+import com.niit.domain.Product;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-//         ApplicationContext applicationContext = new AnnotationCon
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanConfig.class);
+        Product product2 = applicationContext.getBean(Product.class);
+        System.out.println(product2);
     }
 }
